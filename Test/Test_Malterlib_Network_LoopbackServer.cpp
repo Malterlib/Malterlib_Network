9444f980aca65cbc1c9d9700c22f0169018a76a1
Erik Olofsson <erik@olofsson.info>
2016-08-08 00:49:16 +0200

    [Malterlib/Network] Fix test failures

--- Test/Test_Malterlib_Network_LoopbackServer.cpp
+++ Test/Test_Malterlib_Network_LoopbackServer.cpp
@@ -223,14 +223,14 @@ namespace
 					(
 						CNetAddress(ListenV4)
 						, fHandlerFactory(&m_ListenSocketV4, ELoopbackType_Normal)
-						, NNet::ENetFlag_None
+						, NNet::ENetFlag_ReuseAddress
 					)
 				;
 				m_ListenSocketV6.f_Listen
 					(
 						CNetAddress(ListenV6)
 						, fHandlerFactory(&m_ListenSocketV6, ELoopbackType_Normal)
-						, NNet::ENetFlag_None
+						, NNet::ENetFlag_ReuseAddress
 					)
 				;
 			}
@@ -246,14 +246,14 @@ namespace
 					(
 						CNetAddress(ListenV4)
 						, fHandlerFactory(&m_EchoListenSocketV4, ELoopbackType_Echo)
-						, NNet::ENetFlag_None
+						, NNet::ENetFlag_ReuseAddress
 					)
 				;
 				m_EchoListenSocketV6.f_Listen
 					(
 						CNetAddress(ListenV6)
 						, fHandlerFactory(&m_EchoListenSocketV6, ELoopbackType_Echo)
-						, NNet::ENetFlag_None
+						, NNet::ENetFlag_ReuseAddress
 					)
 				;
 			}
@@ -269,14 +269,14 @@ namespace
 					(
 						CNetAddress(ListenV4)
 						, fHandlerFactory(&m_MirrorListenSocketV4, ELoopbackType_Mirror)
-						, NNet::ENetFlag_None
+						, NNet::ENetFlag_ReuseAddress
 					)
 				;
 				m_MirrorListenSocketV6.f_Listen
 					(
 						CNetAddress(ListenV6)
 						, fHandlerFactory(&m_MirrorListenSocketV6, ELoopbackType_Mirror)
-						, NNet::ENetFlag_None
+						, NNet::ENetFlag_ReuseAddress
 					)
 				;
 			}
