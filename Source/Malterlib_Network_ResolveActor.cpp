0bcd5928682f8b21cc06e2b11ede1d408993ae95
Erik Olofsson <erik.olofsson@favro.com>
2019-02-06 17:57:52 +0100

    [Malterlib/Network] Refactor continuation to promise/future

--- Source/Malterlib_Network_ResolveActor.cpp
+++ Source/Malterlib_Network_ResolveActor.cpp
@@ -4,14 +4,14 @@
 
 namespace NMib::NNetwork
 {
-	NConcurrency::TCContinuation<NMib::NNetwork::CNetAddress> CResolveActor::f_Resolve(NStr::CStr const &_Address, NNetwork::ENetAddressType _PreferType)
+	NConcurrency::TCFuture<NMib::NNetwork::CNetAddress> CResolveActor::f_Resolve(NStr::CStr const &_Address, NNetwork::ENetAddressType _PreferType)
 	{
-		NConcurrency::TCContinuation<NMib::NNetwork::CNetAddress> Continuation;
+		NConcurrency::TCPromise<NMib::NNetwork::CNetAddress> Promise;
 
 		if (_Address.f_IsEmpty())
 		{
-			Continuation.f_SetResult(NMib::NNetwork::CNetAddress());
-			return Continuation;
+			Promise.f_SetResult(NMib::NNetwork::CNetAddress());
+			return Promise.f_MoveFuture();
 		}
 		auto &Resolve = mp_PendingResolves.f_Insert();
 
@@ -23,26 +23,26 @@ namespace NMib::NNetwork
 			(
 				_Address
 				, _PreferType
-				, [this, ThisWeak, pResolve, Continuation]()
+				, [this, ThisWeak, pResolve, Promise]()
 				{
 					auto This = ThisWeak.f_Lock();
 					if (!This)
 					{
-						Continuation.f_SetException(DMibErrorInstance("Resolve actor was deleted"));
+						Promise.f_SetException(DMibErrorInstance("Resolve actor was deleted"));
 						return;
 					}
 
 					This
 						(
 							&CActor::f_Dispatch
-							, [this, pResolve, Continuation]
+							, [this, pResolve, Promise]
 							{
 								NNetwork::CNetAddress Address;
 								NStr::CStr Error;
 								if (pResolve->f_GetResult(Address, Error))
-									Continuation.f_SetResult(fg_Move(Address));
+									Promise.f_SetResult(fg_Move(Address));
 								else
-									Continuation.f_SetException(DMibErrorInstance(NStr::CStr(Error)));
+									Promise.f_SetException(DMibErrorInstance(NStr::CStr(Error)));
 
 								mp_PendingResolves.f_Remove(*pResolve);
 							}
@@ -53,6 +53,6 @@ namespace NMib::NNetwork
 			)
 		;
 
-		return Continuation;
+		return Promise.f_MoveFuture();
 	}
 }
